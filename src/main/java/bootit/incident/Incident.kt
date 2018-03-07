7b834bd49dc610274b9f32570f17dc25443dba2b
Olli Salonen <olli.salonen@futurice.com>
2018-03-07 17:38:09 +0100

    Entity to Kotlin

---
+++ src/main/java/bootit/incident/Incident.kt
@@ -0,0 +1,14 @@
+package bootit.incident
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+data class Incident(
+        val what: String,
+        @Id @GeneratedValue val id : Long,
+        val timestamp: Long) {
+    constructor(what: String) : this(what, -1, System.currentTimeMillis())
+    constructor() : this("")
+}
