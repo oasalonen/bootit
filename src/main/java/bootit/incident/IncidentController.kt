ef73405c52c4da764375df1818844148e9d50418
Olli Salonen <olli.salonen@futurice.com>
2018-03-08 12:12:42 +0100

    Kotlin cleanup

--- src/main/java/bootit/incident/IncidentController.kt
+++ src/main/java/bootit/incident/IncidentController.kt
@@ -4,12 +4,10 @@ import bootit.messenger.Messenger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-@RestController class IncidentController {
+@RestController
+class IncidentController {
 
     @Autowired
     lateinit var repository: IncidentRepository
@@ -17,16 +15,22 @@ import org.springframework.web.bind.annotation.RestController
     @Autowired
     lateinit var messenger: Messenger
 
-    @RequestMapping(value = "/incidents", method = [RequestMethod.POST])
+    @PostMapping("/incidents")
     fun addIncident(@RequestParam(value = "what", defaultValue = "") what: String) : ResponseEntity<Incident> {
         val incident = repository.save(Incident(what));
         messenger.send("Added new Incident");
         return ResponseEntity(incident, HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "/incidents", method = [RequestMethod.GET])
+    @GetMapping("/incidents")
     fun getIncidents(@RequestParam(value = "like", defaultValue = "") like: String) = when (like) {
             "" -> repository.findAll()
             else -> repository.findAll().filter { it.what.contains(like) }
     }
+
+    @GetMapping("/incidents/{id}")
+    fun getIncident(@PathVariable id: Long) =
+            repository.findById(id)
+                    .map { ResponseEntity.ok(it) }
+                    .orElse( ResponseEntity(HttpStatus.NOT_FOUND) )
 }
