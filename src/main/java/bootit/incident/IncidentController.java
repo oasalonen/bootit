6edf6b9ef378e42b25e71fad6a330a543bc58ec9
Olli Salonen <olli.salonen@futurice.com>
2018-03-08 10:54:33 +0100

    more restful ifc

--- src/main/java/bootit/incident/IncidentController.java
+++ src/main/java/bootit/incident/IncidentController.java
@@ -2,7 +2,10 @@ package bootit.incident;
 
 import bootit.messenger.Messenger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +18,15 @@ public class IncidentController {
     @Autowired
     private Messenger messenger;
 
-    @RequestMapping("/incident")
-    public Iterable<Incident> incident(@RequestParam(value="what", defaultValue="") String what) {
+    @RequestMapping(value = "/incidents", method = RequestMethod.POST)
+    public ResponseEntity<Incident> addIncident(@RequestParam(value="what", defaultValue="") String what) {
         messenger.send("Added new Incident");
-        repository.save(new Incident(what));
-        return repository.findAll();
+        Incident incident = repository.save(new Incident(what));
+        return new ResponseEntity<Incident>(incident, HttpStatus.CREATED);
     }
 
+    @RequestMapping(value = "/incidents", method = RequestMethod.GET)
+    public Iterable<Incident> getIncidents() {
+        return repository.findAll();
+    }
 }
