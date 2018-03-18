96a89085244c80f6782a942f385a013da04935d7
Olli Salonen <olli.salonen@futurice.com>
2018-03-18 15:08:57 +0100

    Working ELK logging with JSON log payloads

--- src/main/java/bootit/api/ApiController.java
+++ src/main/java/bootit/api/ApiController.java
@@ -1,13 +1,18 @@
 package bootit.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+    private static final Logger log = LoggerFactory.getLogger(ApiController.class);
+
     @RequestMapping("/api")
     public String api() {
+        log.info("API was called");
         return "APIv3!";
     }
 
