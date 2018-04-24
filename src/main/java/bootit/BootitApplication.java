e44e91c2e55a27df10877ad640c5c78eeee0b80d
Olli Salonen <oasalonen@gmail.com>
2018-04-24 15:55:35 +0200

    Redis caching

--- src/main/java/bootit/BootitApplication.java
+++ src/main/java/bootit/BootitApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude=RabbitAutoConfiguration.class)
+@EnableCaching
 public class BootitApplication {
 
     public static void main(String[] args) {
--- src/main/java/bootit/api/ApiController.java
+++ src/main/java/bootit/api/ApiController.java
@@ -3,7 +3,10 @@ package bootit.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,5 +30,13 @@ public class ApiController {
     public void nuke() {
         log.debug("Nuke called");
         System.exit(1);
+    }
+
+    @Cacheable(value = "cache-endpoint", key = "#seed")
+    @RequestMapping("/cache/{seed}")
+    public long cache(@PathVariable int seed) throws InterruptedException {
+        log.debug("Cache miss for " + seed);
+        Thread.sleep(5000);
+        return System.currentTimeMillis();
     }
 }
